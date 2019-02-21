18895e3c9f9f57463522345c9b3e0cd3cda98947
Saulo Oliveira <oliveira.saulo06@gmail.com>
2019-02-20 21:58:58 -0400

    site com ajuste cno menu drop e links da navbar

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@
 
 	var navigation = function() {
 
-		$('body').on('click', '#ubea-offcanvas ul a:true([class="external"]), .main-nav a:not([class="external"])', function(event){
+		$('body').on('click', '#ubea-offcanvas ul a:not([class="external"]), .main-nav a:not([class="external"])', function(event){
 			var section = $(this).data('nav-section');
 				if ( $('[data-section="' + section + '"]').length ) {
 			    	$('html, body').animate({
